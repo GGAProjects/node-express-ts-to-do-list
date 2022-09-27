cb4c2294b2c3ffbf350cf9b82528ebbdff8f2f2c
Gianpierre Galvez <ggalvez92@hotmail.com>
2022-09-27 15:34:14 -0500

    another

--- dist/routes.js
+++ dist/routes.js
@@ -5,5 +5,11 @@ var express_1 = require("express");
 var config_1 = require("./config");
 var router = (0, express_1.Router)();
 exports.router = router;
+router.get("/", function (_req, res) {
+    res.json({
+        status: 200,
+        message: "Testing2"
+    });
+});
 router.use(config_1.setupResponse);
 //# sourceMappingURL=routes.js.map
