edc6df8b93a26ba9025a0760fa7dd9414cbdb758
Gianpierre Galvez <ggalvez92@hotmail.com>
2022-09-26 21:26:40 -0500

    test

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
 require("module-alias/register");
 require("dotenv").config();
 var app = (0, express_1.default)();
+var corsOptions = {};
+app.use((0, cors_1.default)(corsOptions));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
 app.get('/', function (_req, res) {
     res.send('OK');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import express from 'express'
-// import cors from 'cors'
+import cors from 'cors'
 import 'module-alias/register';
 require("dotenv").config();
 
 // import { router } from './routes'
 
 const app = express()
-// const corsOptions = {}
+const corsOptions = {}
 
-// app.use(cors(corsOptions));
-// app.use(express.json())
-// app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 // app.use("/api", router);
 
