b55799c8cd44b816f45d40510670e18331f9e0f1
Gianpierre Galvez <ggalvez92@hotmail.com>
2022-09-27 20:08:21 -0500

    pls work

--- dist/routes.js
+++ dist/routes.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 var express_1 = require("express");
+var routes_1 = require("./modules/user/routes");
 var config_1 = require("./config");
 var router = (0, express_1.Router)();
 exports.router = router;
@@ -12,4 +13,5 @@ router.get("/", function (_req, res) {
     });
 });
 router.use(config_1.setupResponse);
+router.use("/users", routes_1.UserRoutes);
 //# sourceMappingURL=routes.js.map
